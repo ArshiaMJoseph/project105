996c0e76e0e2d38fa89383eb42f983ec3067fefc
ArshiaMJoseph <80831208+ArshiaMJoseph@users.noreply.github.com>
2024-04-20 11:48:38 +0530

    Add files via upload

---
+++ CreateVideo.py
@@ -0,0 +1,14 @@
+path =  "Images/"
+
+Images = []
+
+if ext in ['.gif','.jpg','.png','.gif','.jpeg','.jfif']:
+    file_name = path+"/"+file
+
+    frame = cv2.imread(images[0])
+    frame.shape("width","height","channels")
+
+    size = (width,height)
+    print(size)
+
+    
